8effbd23361c2779b595ce0bb3446e96ff1b32e0
Siwei Yang <yangsiwei880813@gmail.com>
2013-02-20 00:27:19 -0500

    first

---
+++ .bashrc
@@ -0,0 +1,5 @@
+#!/usr/env bash
+alias upgrade="sudo apt-get update;sudo apt-get upgrade -y;"
+
+export PATH=$PATH:~/opt/bin
+
---
+++ init.el
@@ -0,0 +1,20 @@
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(package-initialize)
+(package-refresh-contents)
+
+(unless (package-installed-p 'haskell-mode)
+  (package-install 'haskell-mode))
+
+(unless (package-installed-p 'clojure-mode)
+  (package-install 'clojure-mode))
+
+(unless (package-installed-p 'paredit)
+  (package-install 'paredit))
+
+;; (require 'paredit) if you didn't install it via package.el
+(add-hook 'clojure-mode-hook 'paredit-mode)
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation-mode)
