e5313ca61b0bd856e8e8954e847ef722396c6e40
Siwei Yang <yangsiwei880813@gmail.com>
2015-03-25 23:05:33 -0400

    Update init.el

--- init.el
+++ init.el
@@ -1,15 +1,22 @@
 (require 'package)
 (package-initialize)
 
-(require 'company)
-(require 'rainbow-delimiters)
-
-(add-to-list 'load-path "~/.cabal/share/structured-haskell-mode-1.0.20/elisp/")
-(require 'shm)
-
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 ;(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 ;(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(require 'haskell-interactive-mode)
+(require 'haskell-process)
+(require 'company)
+(require 'rainbow-delimiters)
+;(add-to-list 'load-path "~/.cabal/share/structured-haskell-mode-1.0.20/elisp/")               
+(require 'shm)
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/emacs-color-theme-solarized")
+(load-theme 'solarized t)
 
 ;(unless (package-installed-p 'haskell-mode)
 ;  (package-install 'haskell-mode))
@@ -30,20 +37,28 @@
   (add-to-list 'exec-path my-cabal-path))
 
 (custom-set-variables '(haskell-tags-on-save t))
-(custom-set-variables '(haskell-process-type 'cabal-repl))
+(custom-set-variables '(haskell-process-type 'ghci))
 (custom-set-variables
   '(haskell-process-suggest-remove-import-lines t)
   '(haskell-process-auto-import-loaded-modules t)
   '(haskell-process-log t))
+(eval-after-load 'haskell-mode '(progn
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-c C-z") 'haskell-interactive-switch)
+  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)))
 
 ;; complete haskell-mode hooks
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-decl-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation-mode)
-
+;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation-mode)
 (add-hook 'haskell-mode-hook 'structured-haskell-mode)
+
 (add-hook 'haskell-mode-hook #'rainbow-delimiters-mode)
 
+(add-hook 'haskell-mode-hook 'interactive-haskell-mode)
+
+(add-hook 'haskell-mode-hook 'company-mode)
 (add-to-list 'company-backends 'company-ghc)
 (custom-set-variables '(company-ghc-show-info t))
-(add-hook 'haskell-mode-hook 'company-mode)
+(custom-set-variables '(company-ghc-autoscan t))
